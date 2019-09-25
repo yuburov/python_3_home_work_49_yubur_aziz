20c469c56eb4456bfc6ddf7929e60d55305dc945
Yuburov <yubur.aziz@gmail.com>
2019-09-25 21:10:27 +0600

    Добавил в админ-панель задачи, создал фикстуру

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from webapp.models import Task, Status, Type
 
-# Register your models here.
+admin.site.register(Task)
+admin.site.register(Status)
+admin.site.register(Type)
