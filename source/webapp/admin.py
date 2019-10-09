0fb8ae1623ab81135a1371c7ff4674bf58f280e7
Yuburov <yubur.aziz@gmail.com>
2019-10-09 15:15:06 +0600

    Добавлена модель Project, применена мирация

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from webapp.models import Task, Status, Type
+from webapp.models import Task, Status, Type, Project
 
 admin.site.register(Task)
 admin.site.register(Status)
 admin.site.register(Type)
+admin.site.register(Project)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 class Task(models.Model):
     summary = models.CharField(max_length=200, verbose_name='Краткое описание')
-    description = models.TextField(max_length=2000,null=False,blank=False, verbose_name='Полное описание')
+    description = models.TextField(max_length=2000,null=True,blank=True, verbose_name='Полное описание')
     status = models.ForeignKey('webapp.Status', related_name='tasks', on_delete=models.PROTECT, verbose_name='Статус')
     type = models.ForeignKey('webapp.Type', related_name='tasks', on_delete=models.PROTECT, verbose_name='Тип')
     date_create = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    project = models.ForeignKey('webapp.Project', null= True, blank=False, related_name='tasks', on_delete=models.PROTECT,verbose_name='Проект')
 
     def __str__(self):
         return self.summary
@@ -21,3 +22,12 @@ class Type(models.Model):
 
     def __str__(self):
         return self.type
+
+class Project(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Название проекта')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Описание проекта')
+    create_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    update_date = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+
+    def __str__(self):
+        return self.name
