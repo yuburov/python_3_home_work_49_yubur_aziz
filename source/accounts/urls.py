d0cc6e3c633799cdc30b05ea6a6a33220a9cd2fd
Yuburov <yubur.aziz@gmail.com>
2019-10-27 15:59:23 +0600

    Добавлено представление register view

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from accounts.views import login_view, logout_view
+from accounts.views import login_view, logout_view, register_view
 
 urlpatterns = [
     path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout')
+    path('logout/', logout_view, name='logout'),
+    path('register/', register_view, name='register')
 ]
 
 app_name = 'accounts'
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
+
+from accounts.forms import UserCreationForm
 
 
 def login_view(request):
@@ -22,3 +25,28 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('webapp:index')
+
+
+
+def register_view(request):
+    if request.method == 'GET':
+        form = UserCreationForm()
+        return render(request, 'register.html', {'form': form})
+    elif request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            user = User(
+                username=form.cleaned_data['username'],
+                first_name= form.cleaned_data['first_name'],
+                last_name = form.cleaned_data['last_name'],
+                email=form.cleaned_data['email']
+            )
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            login(request, user)
+            return redirect('webapp:index')
+        else:
+            return render(request, 'register.html', {'form': form})
+
+
+
