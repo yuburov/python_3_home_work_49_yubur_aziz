06fd24f3643f5762b849342af2a23ee3504fd409
Yuburov <yubur.aziz@gmail.com>
2019-10-27 15:57:40 +0600

    Добавил в форму валидатор на ппроверку заполнения хотя бы одного из полей. Также добавил поле email и сделал его обязательным

---
+++ source/accounts/forms.py
@@ -0,0 +1,45 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+class UserCreationForm(forms.Form):
+    username = forms.CharField(max_length=100, required=True, label='Username')
+    first_name = forms.CharField(max_length=100, required=False, label='First name')
+    last_name = forms.CharField(max_length=100, required=False, label='Last name')
+    password = forms.CharField(max_length=100, required=True, label='Password', widget=forms.PasswordInput)
+    password_confirm = forms.CharField(max_length=100, required=True, label='Password confirm',
+                                       widget=forms.PasswordInput)
+    email = forms.EmailField(required=True, label='Email')
+
+    def clean(self):
+        super().clean()
+        password_1 = self.cleaned_data['password']
+        password_2 = self.cleaned_data['password_confirm']
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        if password_1 != password_2:
+            raise ValidationError('Passwords do not match',
+                                  code='passwords_do_not_match')
+        if not first_name and  not last_name:
+            raise ValidationError('Please fill in one of this fields: first name, last name',
+                                  code='empty field')
+        return self.cleaned_data
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        try:
+            User.objects.get(email=email)
+            raise ValidationError('User with this email already exists',
+                                  code='user_email_exists')
+        except User.DoesNotExist:
+            return email
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        try:
+            User.objects.get(username=username)
+            raise ValidationError('User with this username already exists',
+                                  code='user_username_exists')
+        except User.DoesNotExist:
+            return username
+
