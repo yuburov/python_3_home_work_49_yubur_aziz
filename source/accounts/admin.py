ac2aff7689f402ae2e01f68862301f3a519818ab
Yuburov <yubur.aziz@gmail.com>
2019-11-01 18:14:47 +0600

    Модель зарегистрирована в админке

--- source/accounts/admin.py
+++ source/accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from accounts.models import Profile
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    fields = ['avatar', 'about_me', 'github_profile']
+
+
+class ProfileAdmin(UserAdmin):
+    inlines = [ProfileInline]
+
+admin.site.unregister(User)
+admin.site.register(User, ProfileAdmin)
